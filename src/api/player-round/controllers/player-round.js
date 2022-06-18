3cfc9c578591b3614484cdbfd7fa3037e320a895
Alfredo Colantonio <alfredo.colantonio@gruppofilippetti.it>
2022-06-18 19:08:12 +0200

    Added getCurrRound endpoint + plugin swagger documentation.

--- src/api/player-round/controllers/player-round.js
+++ src/api/player-round/controllers/player-round.js
@@ -6,4 +6,32 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::player-round.player-round');
+module.exports = createCoreController('api::player-round.player-round', ({ strapi }) => ({
+    async getCurrRoundUser(ctx) {
+        // some logic here
+        const { id, role } = ctx.state.user;
+        const gameSessionId = ctx.params.gameSessionId;
+
+        const rounds = await strapi.entityService.findMany('api::game-round.game-round', {
+            filters: {
+                game_session: gameSessionId,
+                closed: false,
+            },
+            sort: { n_round: 'ASC' },
+            limit: 1
+        });
+        //const isAdmin = role.type === 'admin';
+
+        const currRound = rounds[0];
+        ctx.query = {
+            ...ctx.query, filters: {
+                user: id,
+                roundGame: currRound.id
+            }
+        }
+        const { data, meta } = await super.find(ctx);
+        // some more logic
+
+        return { data, meta };
+    }
+}));
---
+++ src/api/player-round/routes/custom-player-round.js
@@ -0,0 +1,14 @@
+module.exports = {
+    routes: [
+      { // Path defined with a URL parameter
+        method: 'GET',
+        path: '/player-round/getCurrRound/:gameSessionId',
+        handler: 'player-round.getCurrRoundUser',
+        config: {
+          policies: [],
+          description: "Get current round by session id",
+          
+        }
+      }
+    ]
+}
